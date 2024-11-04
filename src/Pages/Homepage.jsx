8b6ce7a340b91d192738071e0e2c3e39f72df1a3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-04 15:45:46 +0530

    starting third phase with redux

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -5,22 +5,21 @@ import Homecards from "./components/Homecards"
 const Homepage = () => {
     const cardsData = [{
         heading: "Appliances",
-        img: "",
+        img: "https://images.pexels.com/photos/4068316/pexels-photo-4068316.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "appliances"
     }, {
         heading: "Headphones",
-        img: "",
+        img: "https://images.pexels.com/photos/5726030/pexels-photo-5726030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "Headphones"
     }, {
         heading: "Styles for Kids",
-        img: "",
+        img: "https://images.pexels.com/photos/1620760/pexels-photo-1620760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "styling for kids"
     }, {
         heading: "Best Sellers Phones",
-        img: "",
+        img: "https://images.pexels.com/photos/270257/pexels-photo-270257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         alt: "phones"
     }]
-
 
 
     return (
