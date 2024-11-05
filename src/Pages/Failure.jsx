9c59f30599721febc107925ed52c12b8b0a6dac3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-11-05 21:47:51 +0530

    starting the final phase

---
+++ src/Pages/Failure.jsx
@@ -0,0 +1,9 @@
+const Failure = () => {
+    return (
+        <div>
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Failure
---
+++ src/Pages/Success.jsx
@@ -0,0 +1,9 @@
+const Success = () => {
+    return (
+        <div>
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Success
